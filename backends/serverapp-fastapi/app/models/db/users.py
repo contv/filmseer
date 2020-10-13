95992f2b3b3dd6a2e5aa479354c9aff55ab40b18
tom hagis <tom.b.hagis@gmail.com>
2020-10-13 13:23:41 +1100

    adds user and profile image models

--- backends/serverapp-fastapi/app/models/db/users.py
+++ backends/serverapp-fastapi/app/models/db/users.py
@@ -8,11 +8,16 @@ def _new_uuid():
     return to_uuid(id())
 
 
-class User(Model):
-    userid = fields.UUIDField(pk=True, default=_new_uuid, source_field="userID")
+class Users(Model):
+    user_id = fields.UUIDField(pk=True, default=_new_uuid)
     username = fields.CharField(max_length=32)
-    password_hash = fields.CharField(max_length=255, source_field="passwordHash")
-    create_date = fields.DatetimeField(auto_now_add=True, source_field="createDate")
+    password_hash = fields.CharField(max_length=255)
+    profile_image_id = fields.ForeignKeyField(model_name='models.ProfileImages', null=True)
+    description = fields.CharField(max_length=140, null=True)
+    role = fields.CharField(max_length=16, default='user') #may need changing, i.e. Do we want an enum field?
+    last_login_date = fields.DatetimeField(auto_now_add=True)
+    last_login_ip = fields.CharField(max_length=32, null=True)
+    create_date = fields.DatetimeField(auto_now_add=True)
+    delete_date = fields.DatetimeField()
 
-
-__all__ = ["User"]  # This is optional
+__all__ = ["Users"]
