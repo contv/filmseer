e22dbd6220f85ce1510c8524aa47d8068a3cc387
tom hagis <tom.b.hagis@gmail.com>
2020-10-19 16:00:32 +1100

    - removes regex trailer parsing and replaces with library-supported json parsing from postgres json field - cleans up code and naming - removes unused imports

--- backends/serverapp-fastapi/app/api/v1/routers/movie.py
+++ backends/serverapp-fastapi/app/api/v1/routers/movie.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
 from typing import Optional, List
-import re
 
 from app.models.db.movies import Movies
 from app.models.db.positions import Positions
@@ -35,33 +34,13 @@ async def get_movie(movie_id: str):
     if movie is None:
         return ApiException(404, 2100, "That movie doesn't exist")
 
-    trailers = []
-    trailer_re = r"key:(.+), *site:(.+)}"
-
-    if movie.trailer is not None:
-        # extracts key and site values from trailer entries
-        # in form "{key:k1,site:s1},{key:k2,site:s2}..."
-        trailers_split = movie.trailer.split(",{")
-        for t in trailers_split:
-            m = re.search(trailer_re, t)
-            key = str(m.group(1))
-            site = str(m.group(2))
-            trailer = dict({"key": key, "site": site})
-            trailers.append(trailer)
-
-    crew = []
-    movie_crew = await Positions.filter(
-        movie_id=movie_id).prefetch_related("person")
-
-    for p in movie_crew:
-        crew.append(
-            {
+    crew = [{
                 "id": p.person_id,
                 "name": p.person.name,
                 "position": p.position,
                 "image": p.person.image,
-            }
-        )
+            } for p in await Positions.filter(
+        movie_id=movie_id).prefetch_related("person")]
 
     movie_detail = {
         "id": str(movie.movie_id),
@@ -70,7 +49,7 @@ async def get_movie(movie_id: str):
         "release_year": str(movie.release_date.year),
         "description": movie.description,
         "image_url": movie.image,
-        "trailers": trailers,
+        "trailers": movie.trailer,
         "num_reviews": movie.num_reviews,
         "num_votes": movie.num_votes,
         "cumulative_rating": movie.cumulative_rating,
