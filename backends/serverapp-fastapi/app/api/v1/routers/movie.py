e8064fc8559105c0ab542d131d9059f0caf67c60
tom hagis <tom.b.hagis@gmail.com>
2020-10-20 09:23:16 +1100

    wip

--- backends/serverapp-fastapi/app/api/v1/routers/movie.py
+++ backends/serverapp-fastapi/app/api/v1/routers/movie.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
 from typing import Optional, List
+from humps import camelize
 
 from app.models.db.movies import Movies
 from app.models.db.positions import Positions
@@ -24,9 +25,12 @@ class MovieResponse(BaseModel):
     average_rating: float
     crew: List[dict]
 
+    class Config:
+        alias_generator = camelize
+
 
 def calc_average_rating(cumulative_rating, num_votes):
-    return round(cumulative_rating/num_votes if num_votes > 0 else 0, 1)
+    return round(cumulative_rating / num_votes if num_votes > 0 else 0, 1)
 
 
 @router.get("/{movie_id}/ratings")
@@ -49,31 +53,35 @@ async def get_movie(movie_id: str):
     if movie is None:
         return ApiException(404, 2100, "That movie doesn't exist")
 
-    crew = [{
-                "id": p.person_id,
-                "name": p.person.name,
-                "position": p.position,
-                "image": p.person.image,
-            } for p in await Positions.filter(
-        movie_id=movie_id).prefetch_related("person")]
+    crew = [
+        {
+            "id": p.person_id,
+            "name": p.person.name,
+            "position": p.position,
+            "image": p.person.image,
+        }
+        for p in await Positions.filter(
+            movie_id=movie_id
+        ).prefetch_related("person")
+    ]
 
     # TODO remove ratings from blocked users
     average_rating = calc_average_rating(
         movie.cumulative_rating, movie.num_votes
     )
 
-    movie_detail = {
-        "id": str(movie.movie_id),
-        "title": movie.title,
-        "release_date": str(movie.release_date),
-        "release_year": str(movie.release_date.year),
-        "description": movie.description,
-        "image_url": movie.image,
-        "trailers": movie.trailer,
-        "num_reviews": movie.num_reviews,
-        "num_votes": movie.num_votes,
-        "average_rating": average_rating,
-        "crew": crew,
-    }
+    movie_detail = MovieResponse(
+        id=str(movie.movie_id),
+        title=movie.title,
+        release_date=str(movie.release_date),
+        release_year=str(movie.release_date.year),
+        description=movie.description,
+        image_url=movie.image,
+        trailers=movie.trailer,
+        num_reviews=movie.num_reviews,
+        num_votes=movie.num_votes,
+        average_rating=average_rating,
+        crew=crew,
+    )
 
     return wrap(movie_detail)
