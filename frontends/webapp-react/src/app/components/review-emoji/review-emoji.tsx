f976a02ec67e2467831d264feb3fb900528f6337
z5234456 <z5234456@ad.unsw.edu.au>
2020-10-21 03:22:33 +0000

    Initial Placeholder

---
+++ frontends/webapp-react/src/app/components/review-emoji/review-emoji.tsx
@@ -0,0 +1,20 @@
+import { view } from "@risingstack/react-easy-state";
+import React from "react";
+import "./review_emoji.scss";
+
+type ReviewEmojiProps = {
+  helpfulPressed?: boolean;
+  funnyPressed?: boolean;
+  spoilerPressed?: boolean;
+  helpfulCount?: number;
+  funnyCount?: number;
+  spoilerCount?: number;
+};
+
+const ReviewEmoji = (props: ReviewEmojiProps & { className?: string }) => {
+  return (
+    <div className={`ReviewEmoji ${(props.className || "").trim()}`}></div>
+  );
+};
+
+export default view(ReviewEmoji);
