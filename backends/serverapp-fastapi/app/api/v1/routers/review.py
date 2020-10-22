5ebfa365e25e73b761285e26308fa740f919bbf1
z5234456 <z5234456@ad.unsw.edu.au>
2020-10-22 09:36:32 +0000

    return number of reviews after post/delete

--- backends/serverapp-fastapi/app/api/v1/routers/review.py
+++ backends/serverapp-fastapi/app/api/v1/routers/review.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Request
 from tortoise.exceptions import OperationalError
-from tortoise.transactions import atomic, in_transaction
+from tortoise.transactions import in_transaction
 
 from app.models.db.helpful_votes import HelpfulVotes
 from app.models.db.funny_votes import FunnyVotes
@@ -19,15 +19,16 @@ async def mark_review_helpful(request: Request, id: str) -> Wrapper[dict]:
     user_id = request.session.get("user_id")
     if not user_id:
         return ApiException(
-            401, 2500, "You must be logged in to mark the review as spoiler."
+            401, 2500, "You must be logged in to mark the review as helpful."
         )
     try:
         async with in_transaction():
-            await HelpfulVotes(review_id=id, user_id=user_id).get_or_create()
-            # PENDING Update helpful vote count in Reviews?
+            await HelpfulVotes.get_or_create(review_id=id, user_id=user_id)
+        num_helpful = await HelpfulVotes.filter(review_id=id).count()
+        # PENDING Update helpful vote count in Reviews?
     except OperationalError:
         return ApiException(401, 2501, "An exception occurred")
-    return wrap({})
+    return wrap(num_helpful)
 
 
 @router.post("/{id}/funny")
@@ -35,15 +36,16 @@ async def mark_review_funny(request: Request, id: str) -> Wrapper[dict]:
     user_id = request.session.get("user_id")
     if not user_id:
         return ApiException(
-            401, 2500, "You must be logged in to mark the review as spoiler."
+            401, 2500, "You must be logged in to mark the review as funny."
         )
     try:
         async with in_transaction():
-            await FunnyVotes(review_id=id, user_id=user_id).get_or_create()
-            # PENDING Update funny vote count in Reviews?
+            await FunnyVotes.get_or_create(review_id=id, user_id=user_id)
+        num_funny = await FunnyVotes.filter(review_id=id).count()
+        # PENDING Update funny vote count in Reviews?
     except OperationalError:
         return ApiException(401, 2501, "An exception occurred")
-    return wrap({})
+    return wrap(num_funny)
 
 
 @router.post("/{id}/spoiler")
@@ -55,16 +57,12 @@ async def mark_review_spoiler(request: Request, id: str) -> Wrapper[dict]:
         )
     try:
         async with in_transaction():
-            await SpoilerVotes(review_id=id, user_id=user_id).get_or_create()
-        #    count_spoiler = await SpoilerVotes.filter(review_id=id).count()
-        #    if count_spoiler >= 10:
-        #        await Reviews.filter(review_id=id).update(contains_spoiler=True)
-        #    else:
-        #        await Reviews.filter(review_id=id).update(contains_spoiler=False)
-        #    # PENDING Update spoiler vote count in Reviews?
+            await SpoilerVotes.get_or_create(review_id=id, user_id=user_id)
+        num_spoiler = await SpoilerVotes.filter(review_id=id).count()
+        # PENDING Update spoiler vote count in Reviews?
     except OperationalError:
         return ApiException(401, 2501, "An exception occurred")
-    return wrap({})
+    return wrap(num_spoiler)
 
 
 @router.delete("/{id}/helpful")
@@ -72,15 +70,16 @@ async def unmark_review_helpful(request: Request, id: str) -> Wrapper[dict]:
     user_id = request.session.get("user_id")
     if not user_id:
         return ApiException(
-            401, 2500, "You must be logged in to mark the review as spoiler."
+            401, 2500, "You must be logged in to unmark the review as helpful."
         )
     try:
         async with in_transaction():
             await HelpfulVotes.filter(review_id=id, user_id=user_id).delete()
-            # PENDING Update helpful vote count in Reviews?
+        num_helpful = await HelpfulVotes.filter(review_id=id).count()
+        # PENDING Update helpful vote count in Reviews?
     except OperationalError:
         return ApiException(401, 2501, "An exception occurred")
-    return wrap({})
+    return wrap(num_helpful)
 
 
 @router.delete("/{id}/funny")
@@ -88,15 +87,16 @@ async def unmark_review_funny(request: Request, id: str) -> Wrapper[dict]:
     user_id = request.session.get("user_id")
     if not user_id:
         return ApiException(
-            401, 2500, "You must be logged in to mark the review as spoiler."
+            401, 2500, "You must be logged in to unmark the review as funny."
         )
     try:
         async with in_transaction():
             await FunnyVotes.filter(review_id=id, user_id=user_id).delete()
-            # PENDING Update funny vote count in Reviews?
+        num_funny = await FunnyVotes.filter(review_id=id).count()    
+        # PENDING Update funny vote count in Reviews?
     except OperationalError:
         return ApiException(401, 2501, "An exception occurred")
-    return wrap({})
+    return wrap(num_funny)
 
 
 @router.delete("/{id}/spoiler")
@@ -104,16 +104,13 @@ async def unmark_review_spoiler(request: Request, id: str) -> Wrapper[dict]:
     user_id = request.session.get("user_id")
     if not user_id:
         return ApiException(
-            401, 2500, "You must be logged in to mark the review as spoiler."
+            401, 2500, "You must be logged in to unmark the review as spoiler."
         )
     try:
         async with in_transaction():
             await SpoilerVotes.filter(review_id=id, user_id=user_id).delete()
-        #    count_spoiler = await SpoilerVotes.filter(review_id=id).count()
-        #    if count_spoiler >= 10:
-        #        await Reviews.filter(review_id=id).update(contains_spoiler=True)
-        #    else:
-        #        await Reviews.filter(review_id=id).update(contains_spoiler=False)
+        num_spoiler = await SpoilerVotes.filter(review_id=id).count()    
+        # PENDING Update spoiler vote count in Reviews?
     except OperationalError:
         return ApiException(401, 2501, "An exception occurred")
-    return wrap({})
+    return wrap(num_spoiler)
