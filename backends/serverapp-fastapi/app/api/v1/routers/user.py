46706c8832f72f7aa2e29bc0347636f0ef8b685b
tom hagis <tom.b.hagis@gmail.com>
2020-10-23 12:57:57 +1100

    adds placeholder endpoints for wishlist

--- backends/serverapp-fastapi/app/api/v1/routers/user.py
+++ backends/serverapp-fastapi/app/api/v1/routers/user.py
@@ -25,21 +25,7 @@ async def create_user(register: Register, request: Request) -> Wrapper[dict]:
     await Users(username=register.username, password_hash=hash(register.password)).save()
     return wrap({})
 
+
 @router.get("/{username}/wishlist")
 async def get_user_wishlist(username: str):
     return wrap([])
-
-# @router.get("/{username}/wishlist")
-# async def get_user_wishlist(username: str):
-#     user = await Users.filter(username=username).first()
-
-#     if not user:
-#         return ApiException(
-#             404, 2031, "That user doesn't exist."
-#         )
-
-#     wishlist = await Wishlists.filter(
-#         user_id=user.user_id
-#     ).prefetch_related("movie")
-
-#     return wrap(wishlist)
