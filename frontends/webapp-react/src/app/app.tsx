42ea4e216905197ca37f9f320ef5538e463386b5
contv <ctv.sys@gmail.com>
2020-10-25 20:44:57 +1100

    Removed dummy data from app.tsx

--- frontends/webapp-react/src/app/app.tsx
+++ frontends/webapp-react/src/app/app.tsx
@@ -1,3 +1,4 @@
+import { StylesProvider } from "@material-ui/core/styles";
 import { view } from "@risingstack/react-easy-state";
 import React from "react";
 import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
@@ -7,19 +8,6 @@ import UserMenu from "./components/user-menu";
 import logo from "./logo.svg";
 import HomePage from "./routes/home";
 import UserPage from "./routes/user";
-import { StylesProvider } from "@material-ui/core/styles";
-import MovieItem from "./components/movie-item"
-
-const MovieItemProps = {
-  movieId: 'ERGERTHR@#%!@',
-  title: "The Hell's Evil",
-  year: 2345,
-  genres: [{id:"3454", text:'Historical'}, {id:"275", text:'Thriller'}, {id:"867", text:'New Age'}],
-  imageUrl: "https://a.ltrbxd.com/resized/film-poster/5/7/7/5/5/5/577555-the-wolf-of-snow-hollow-0-230-0-345-crop.jpg?k=4918de4b9c",
-  cumulativeRating: 5343,
-  numRatings: 2666,
-  numReviews: 345
-};
 
 const App = () => {
   return (
@@ -38,9 +26,6 @@ const App = () => {
             ></SearchBar>
             <UserMenu className="Header__user-menu" />
           </header>
-
-          <MovieItem {...MovieItemProps} />
-
           <article className="App__main Main">
             <Switch>
               <Route path="/user/:username?">
