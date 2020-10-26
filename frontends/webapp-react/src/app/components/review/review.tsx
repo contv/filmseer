56dbb3400a7571eed416bdd1d9c91512d3f0be3f
tom hagis <tom.b.hagis@gmail.com>
2020-10-27 10:19:02 +1100

    wip

--- frontends/webapp-react/src/app/components/review/review.tsx
+++ frontends/webapp-react/src/app/components/review/review.tsx
@@ -1,21 +1,43 @@
 import { view } from "@risingstack/react-easy-state";
 import React from "react";
-import { ReviewFlagsProps } from "src/app/components/review-flags/review-flags"
+import ReviewFlags from "src/app/components/review-flags";
+import { ReviewFlagsProps } from "src/app/components/review-flags/review-flags";
 import "./review_flags.scss";
 
 type ReviewProps = {
-  reviewId: string;
-  reviewText: string;
-  reviewerUsername: string;
-  reviewerImageUrl?: string;
-  reviewDate: Date;
+  id: string;
+  text: string;
+  username: string;
+  profileImage?: string;
+  date: Date;
+  rating: Number;
+  containsSpoiler: Boolean;
   flags?: ReviewFlagsProps;
 };
 
 const Review = (props: ReviewProps & { className?: string }) => {
-  return (
-    <div className={`Review ${(props.className || "").trim()}`}></div>
-  );
+  return props.flags ? (
+    <div className={`Review ${(props.className || "").trim()}`}>
+      <img src={props.profileImage} width={60} />
+      <div>
+        <i>
+          posted at {props.date.toDateString()} - {props.rating} stars{" "}
+        </i>
+        <p>
+          {props.username} : {props.text}
+        </p>
+      </div>
+      <ReviewFlags
+        reviewId={props.id}
+        flaggedHelpful={props.flags.flaggedHelpful}
+        flaggedFunny={props.flags.flaggedFunny}
+        flaggedSpoiler={props.flags.flaggedHelpful}
+        numHelpful={props.flags.numHelpful}
+        numFunny={props.flags.numFunny}
+        numSpoiler={props.flags.numSpoiler}
+      />
+    </div>
+  ) : null;
 };
 
 export default view(Review);
