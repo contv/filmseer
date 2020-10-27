afcd6b61bd5252ff7b68b519cbce47255b9df373
tom hagis <tom.b.hagis@gmail.com>
2020-10-27 13:02:54 +1100

    review component displays without complete styling

--- frontends/webapp-react/src/app/components/review/review.tsx
+++ frontends/webapp-react/src/app/components/review/review.tsx
@@ -2,9 +2,9 @@ import { view } from "@risingstack/react-easy-state";
 import React from "react";
 import ReviewFlags from "src/app/components/review-flags";
 import { ReviewFlagsProps } from "src/app/components/review-flags/review-flags";
-import "./review_flags.scss";
+import "./review.scss";
 
-type ReviewProps = {
+export type ReviewProps = {
   id: string;
   text: string;
   username: string;
@@ -16,28 +16,38 @@ type ReviewProps = {
 };
 
 const Review = (props: ReviewProps & { className?: string }) => {
-  return props.flags ? (
+  console.log(props.flags)
+  return (
     <div className={`Review ${(props.className || "").trim()}`}>
-      <img src={props.profileImage} width={60} />
-      <div>
-        <i>
-          posted at {props.date.toDateString()} - {props.rating} stars{" "}
-        </i>
-        <p>
-          {props.username} : {props.text}
-        </p>
-      </div>
-      <ReviewFlags
-        reviewId={props.id}
-        flaggedHelpful={props.flags.flaggedHelpful}
-        flaggedFunny={props.flags.flaggedFunny}
-        flaggedSpoiler={props.flags.flaggedHelpful}
-        numHelpful={props.flags.numHelpful}
-        numFunny={props.flags.numFunny}
-        numSpoiler={props.flags.numSpoiler}
+      <a href={`/user/${props.username}`}><img
+        className="ReviewerProfilePicture"
+        src={props.profileImage}
+        width={60}
       />
+      </a>
+      <div className="ReviewContent">
+        <p className="ReviewMeta">
+          {props.rating} stars -{" "}
+          <a href={`/user/${props.username}`}>{props.username}</a>{" "}
+          <span className="ReviewDate">
+            posted at {`${props.date.toUTCString()}`}
+          </span>
+        </p>
+        <p className="ReviewContent">{props.text}</p>
+      </div>
+      {props.flags && (
+        <ReviewFlags
+          reviewId={props.id}
+          flaggedHelpful={props.flags.flaggedHelpful}
+          flaggedFunny={props.flags.flaggedFunny}
+          flaggedSpoiler={props.flags.flaggedHelpful}
+          numHelpful={props.flags.numHelpful}
+          numFunny={props.flags.numFunny}
+          numSpoiler={props.flags.numSpoiler}
+        />
+      )}
     </div>
-  ) : null;
+  );
 };
 
 export default view(Review);
