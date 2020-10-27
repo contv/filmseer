5c78979faa8bf60d81319c6a1e31a152b0e58f44
contv <ctv.sys@gmail.com>
2020-10-27 22:52:51 +1100

    Formatting

--- backends/serverapp-fastapi/app/api/v1/routers/movie.py
+++ backends/serverapp-fastapi/app/api/v1/routers/movie.py
@@ -60,7 +60,6 @@ class SearchResponse(BaseModel):
 class RatingResponse(BaseModel):
     id: str
     rating: float
-      
 
 def calc_average_rating(cumulative_rating, num_votes) -> float:
     return round(cumulative_rating / num_votes if num_votes > 0 else 0.0, 1)
