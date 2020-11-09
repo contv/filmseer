044d5ebffa40949a801f279fca6b27739f9e22c8
Greendam Wang <0w0@wdhwg001.me>
2020-11-09 18:54:43 +1100

    WIP restyling search page

--- frontends/webapp-react/src/app/routes/search/search.tsx
+++ frontends/webapp-react/src/app/routes/search/search.tsx
@@ -3,8 +3,8 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Filter from "src/app/components/filter";
 import MovieItem from "src/app/components/movie-item/movie-item";
+import movieLogo from "src/app/components/movie-item/movie-logo.png";
 import TileList from "src/app/components/tile-list";
-import movieLogo from "src/app/components/movie-item/movie-logo.png"
 import { api } from "src/utils";
 import "./search.scss";
 
@@ -35,6 +35,7 @@ const SearchPage = (props: { className?: string }) => {
   const [groupedResults, setGroupedResults] = useState<
     Array<Array<SearchItem>>
   >();
+  const [movies, setMovies] = useState<SearchItem[]>([]);
   const [hasError, setHasError] = useState<Boolean>(false);
   const [genreFilter, setGenreFilter] = useState<string>();
   const [directorFilter, setDirectorFilter] = useState<string>();
@@ -69,31 +70,43 @@ const SearchPage = (props: { className?: string }) => {
 
   useEffect(() => {
     api({
-      path: `/movie/?keywords=${searchString}${
-        genreFilter ? `&genres=${encodeURI(genreFilter)}` : ""
-      }${
-        directorFilter ? `&directors=${encodeURI(directorFilter)}` : ""
-      }${
-        yearFilter ? `&years=${yearFilter}` : ""
-      }&per_page=80&page=1&sort=${sortBy}&desc=${descending}`,
+      path: "/movie/",
       method: "GET",
+      params: {
+        keywords: searchString,
+        genres: genreFilter,
+        directors: directorFilter,
+        years: yearFilter,
+        per_page: 80,
+        page: 1,
+        sort: sortBy,
+        desc: descending,
+      },
     }).then((res) => {
       if (res.code !== 0) {
         setHasError(true);
       } else {
-        console.log(res.data)
+        console.log(res.data);
+        setMovies(res.data.movies as SearchItem[]);
         const grouped = groupItems(res.data.movies, 8, 10);
         setGroupedResults(grouped);
         setFilters(res.data.filters);
       }
     });
-  }, [searchString, genreFilter, directorFilter, sortBy, descending, yearFilter]);
+  }, [
+    searchString,
+    genreFilter,
+    directorFilter,
+    sortBy,
+    descending,
+    yearFilter,
+  ]);
 
-  if (groupedResults && filters) {
+  if (movies && filters) {
     return (
       <div className={`SearchPage ${(props.className || "").trim()}`}>
         <h3>Search results for "{searchString}"</h3>
-        <div className="Search__filters">
+        <div className="SearchPage__filters">
           {filters &&
             filters.map((filter) => (
               <Filter
@@ -106,7 +119,7 @@ const SearchPage = (props: { className?: string }) => {
               />
             ))}
         </div>
-        <div className="Search__sort">
+        <div className="SearchPage__sort">
           <label htmlFor="sort">Sort by</label>
           <select
             name="sort"
@@ -127,28 +140,27 @@ const SearchPage = (props: { className?: string }) => {
             <option value="ascending">Ascending</option>
           </select>
         </div>
-        {groupedResults &&
-          groupedResults.map((group) => (
-            <TileList
-              items={group.map((movie) => (
-                <div className="Search__item">
-                  <MovieItem
-                    movieId={movie.id}
-                    year={movie.releaseYear}
-                    title={movie.title}
-                    genres={movie.genres.map((g) => ({
-                      id: `${g}-${movie.id}`,
-                      text: g,
-                    }))}
-                    imageUrl={movie.imageUrl || movieLogo}
-                    cumulativeRating={78}
-                    numRatings={20}
-                    numReviews={3}
-                  />
-                </div>
-              ))}
-            />
-          ))}
+        {movies && (
+          <TileList
+            className="SearchPage__list"
+            itemClassName="SearchPage__item"
+            items={movies.map((movie) => (
+              <MovieItem
+                movieId={movie.id}
+                year={movie.releaseYear}
+                title={movie.title}
+                genres={movie.genres.map((g) => ({
+                  id: `${g}-${movie.id}`,
+                  text: g,
+                }))}
+                imageUrl={movie.imageUrl || movieLogo}
+                cumulativeRating={78}
+                numRatings={20}
+                numReviews={3}
+              />
+            ))}
+          />
+        )}
       </div>
     );
   }
